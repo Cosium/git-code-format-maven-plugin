debfb93a03f03ef3d62a41b9b8c596681fac3b0f
Raghavendra Bhuvan <rage28@gmail.com>
2018-07-27 15:08:08 +0530

    Filter out deleted files before formatting

--- src/main/resources/com/cosium/code/format/maven-git-code-format.pre-commit.sh
+++ src/main/resources/com/cosium/code/format/maven-git-code-format.pre-commit.sh
@@ -2,7 +2,7 @@
 set -e
 # Retrieve staged files
 STAGED_FILES_FILE=$(mktemp)
-git diff --cached --name-only > "${STAGED_FILES_FILE}"
+git diff --cached --name-only --diff-filter=d > "${STAGED_FILES_FILE}"
 # Process the files
 %s git-code-format:on-pre-commit -DstagedFilesFile=${STAGED_FILES_FILE} %s
 # Add the files to staging again in case they were modified by the process
