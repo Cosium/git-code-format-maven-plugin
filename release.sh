36888e277459e9a0d1d7edcc199891dd31ce49ea
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2019-06-29 19:08:45 +0200

    Stop using maven-release-plugin

--- release.sh
+++ release.sh
@@ -1,2 +1,22 @@
 #!/usr/bin/env bash
-mvn --batch-mode clean release:prepare release:perform && git push && git push --tags
+set -e
+
+mvn versions:set -DremoveSnapshot=true
+mvn versions:commit 
+git add pom.xml
+
+RELEASE_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+git commit -m "Release $RELEASE_VERSION" 
+git tag ${RELEASE_VERSION}
+mvn clean deploy -DperformRelease=true
+
+mvn versions:set -DnextSnapshot=true
+mvn versions:commit 
+git add pom.xml
+
+NEW_SNAPSHOT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+git commit -m "Setup $NEW_SNAPSHOT_VERSION"
+
+git push
+git push --tags
