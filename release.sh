758fd8f3adfb5daed1288a924adad7223f2c8f97
Réda Housni Alaoui <reda.housnialaoui@gmail.com>
2019-06-30 16:38:51 +0200

    Separate test from deploy

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+mvn clean test
+
 mvn versions:set -DremoveSnapshot=true
 mvn versions:commit 
 git add pom.xml
@@ -8,7 +10,7 @@ git add pom.xml
 RELEASE_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 git commit -m "Release $RELEASE_VERSION" 
-mvn clean deploy -DperformRelease=true
+mvn clean deploy -DperformRelease=true -DskipTests
 git tag ${RELEASE_VERSION}
 
 mvn versions:set -DnextSnapshot=true
