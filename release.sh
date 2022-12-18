ea1701eb38bedc56df62dc0e44c2f9512b69521e
Réda Housni Alaoui <reda-alaoui@hey.com>
2022-12-18 14:37:43 +0100

    Simplify release.sh

--- release.sh
+++ release.sh
@@ -1,24 +1,2 @@
 #!/usr/bin/env bash
-set -e
-
-./mvnw clean test
-
-./mvnw versions:set -DremoveSnapshot=true
-./mvnw versions:commit
-git add pom.xml
-
-RELEASE_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
-
-git commit -m "Release $RELEASE_VERSION" 
-./mvnw clean deploy -DperformRelease=true -DskipTests
-git tag ${RELEASE_VERSION}
-
-./mvnw versions:set -DnextSnapshot=true
-./mvnw versions:commit
-git add pom.xml
-
-NEW_SNAPSHOT_VERSION=$(./mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
-git commit -m "Setup $NEW_SNAPSHOT_VERSION"
-
-git push
-git push --tags
+./mvnw --batch-mode clean release:prepare release:perform && git push && git push --tags
